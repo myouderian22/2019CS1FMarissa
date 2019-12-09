b79c3c18249b6c7c2a1fe6cc51d64850fb1e6cda
myouderian22 <myouderian22@nscds.org>
2019-12-09 19:00:06 +0000

    Experimenting

--- Day4/miniCalc.js
+++ Day4/miniCalc.js
@@ -5,8 +5,10 @@ $('#sumButton').click(
          * HINT: parseInt() -  https://www.w3schools.com/jsref/jsref_parseint.asp 
          * 
          **/
+        
         var a = parseInt($('#sumA').val());
         var b = parseInt($('#sumB').val());
+
         var sum = a + b;
         $('#sumTotal').text(sum);
     }
