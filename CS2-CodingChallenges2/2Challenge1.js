b2d0cb38050f7193ee4cfcacf0ee6602d4c8641b
myouderian22 <myouderian22@nscds.org>
2020-01-21 20:21:55 +0000

    new files

---
+++ CS2-CodingChallenges2/2Challenge1.js
@@ -0,0 +1,23 @@
+function centuryFromYear(a, b) {
+    return Math.ceil(a,b)
+}
+
+
+var a = 1905;
+var b = 1700;
+
+console.log(centuryFromYear(1905));
+console.log(centuryFromYear(1700));
+
+
+
+
+/*
+function add(param1, param2){
+    return param1 + param2;
+}
+
+var a = 3;
+var b = 5;
+
+console.log(a + "+" + b + " = " + add(a,b)); */
