38f8ef5ec9cdf5d5de1b9207d5f0b28275743867
myouderian22 <myouderian22@nscds.org>
2020-02-06 17:40:37 +0000

    adding

--- TicTacToe/TicTacToe.js
+++ TicTacToe/TicTacToe.js
@@ -13,16 +13,42 @@ init();
 function init(){
     for(var c=0; c<3; c++)
         for(var r=0; r<3; r++){
-            $('.box').append("<div class='pixel'>" + arrayExample[c][r] + "</div>");
+            $('.box').append("<div class='pixel' player='-1'>" + arrayExample[c][r] + "</div>");
         }
 }
 
+var color1= 'red';
+var color2= 'blue';
+var color3= 'rgb(255,255,255)';
+var numClicks= '0';
+
+
 
 $('.pixel').click(
-    function(){
-        var color= 'blue';
-        console.log(color);
-        $(this).css("background-color",color);
+    function() {
+
+        var player= numClicks%2;
+
+        var currentColor = $(this).css("background-color");
+        console.log(player);
+
+        
+
+        //if(currentColor==color3){
+
+            if(player==1) {
+                console.log(color1);
+                $(this).css("background-color",color1);   
+            }
+            else{
+                console.log(color2);
+                $(this).css("background-color",color2);  
+            }
+            numClicks++;
+            console.log(numClicks);
+       // }
+        //else{
+           // console.log("Something Went Wrong");
+        //}
     }
 );
-
