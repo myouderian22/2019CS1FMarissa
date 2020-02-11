5d7ac28e9fc19f8d92944c3589b4a9e1a30f61be
myouderian22 <myouderian22@nscds.org>
2020-02-11 20:57:06 +0000

    new

--- TicTacToe/TicTacToe.js
+++ TicTacToe/TicTacToe.js
@@ -11,17 +11,15 @@ var arrayExample =
 init();
 
 function init(){
-    for(var c=0; c<3; c++)
-        for(var r=0; r<3; r++){
-            $('.box').append("<div class='pixel' player='-1'>" + arrayExample[c][r] + "</div>");
+    for(var r=0; r<3; r++)
+        for(var c=0; c<3; c++){
+            $('.box').append("<div class='pixel' r='" + r + "' c= '" + c + "' player='-1'>" + arrayExample[c][r] + "</div>");
         }
 }
 
 var color1= 'red';
 var color2= 'blue';
-var color3= 'rgb(255,255,255)';
 var numClicks= '0';
-
 
 
 $('.pixel').click(
@@ -29,26 +27,22 @@ $('.pixel').click(
 
         var player= numClicks%2;
 
-        var currentColor = $(this).css("background-color");
-        console.log(player);
+        var currentPlayer = $(this).attr("player");
+        console.log(currentPlayer);
 
-        
-
-        //if(currentColor==color3){
+        if(currentPlayer=="-1"){
 
             if(player==1) {
-                console.log(color1);
                 $(this).css("background-color",color1);   
             }
             else{
-                console.log(color2);
                 $(this).css("background-color",color2);  
+                
             }
+            $(this).attr("player",player);
             numClicks++;
-            console.log(numClicks);
-       // }
-        //else{
-           // console.log("Something Went Wrong");
-        //}
+       }
+       if()
     }
 );
+
