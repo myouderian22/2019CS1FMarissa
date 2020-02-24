73349a62498cd57b969a794b241d62c214268197
myouderian22 <myouderian22@nscds.org>
2020-02-24 21:08:27 +0000

    Finished tictactoe and made new files

--- TicTacToe/TicTacToe.js
+++ TicTacToe/TicTacToe.js
@@ -1,19 +1,9 @@
-var arrayExample = 
-
-    [
-    ['','',''],
-    ['','',''],
-    ['','',''],
-    ]
-   ;
-
-   
 init();
 
 function init(){
     for(var r=0; r<3; r++)
         for(var c=0; c<3; c++){
-            $('.box').append("<div class='pixel' r= '" + r + "'  c= '" + c + "' player='-1'>" + arrayExample[c][r] + "</div>");
+            $('.box').append("<div class='pixel' r= '" + r + "'  c= '" + c + "' p='-1'></div>");
         }
 }
 
@@ -21,6 +11,26 @@ var color1 = 'red';
 var color2 = 'blue';
 var numClicks = '0';
 
+function hasWon(box){
+    var row = $(box).attr("r") ;
+    var col = $(box).attr("c") ;
+    var player = $(box).attr("p");
+    
+    var rowClicked = $('[r='+row+'][p='+player+']');
+    console.log(rowClicked.length);
+    var colClicked = $('[c='+col+'][p='+player+']');
+    console.log(colClicked.length);
+
+    console.log(row.length);
+    console.log(col.length);
+    console.log(row, col, player);
+    if(rowClicked.length>=3){
+        alert("you win!");
+    }
+    if(colClicked.length>=3){
+        alert("you win!");
+    }
+}
 
 $('.pixel').click(
     function() {
@@ -29,7 +39,7 @@ $('.pixel').click(
 
         var player = numClicks%2;
 
-        var currentPlayer = $(this).attr("player");
+        var currentPlayer = $(this).attr("p");
         console.log(currentPlayer);
 
         if(currentPlayer=="-1"){
@@ -41,18 +51,9 @@ $('.pixel').click(
                 $(this).css("background-color",color2);  
                 
             }
-            $(this).attr("player",player);
+            $(this).attr("p",player);
             numClicks++;
        }
-       if(hasWon(this))
-        alert("you win");
+       hasWon(this);
     }
 );
-
-function hasWon(box){
-    var row = $(box).attr("r") ;
-    var col = $(box).attr("c") ;
-    var player = $(box).attr("player");
-    console.log(row, col, player);
-        if($('[r=0][p=0]').length>='3') {
-}
